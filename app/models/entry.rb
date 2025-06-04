3aa9a16854570fdfc8b14d64aab9c32919c407a1
Harrison Schroeder <harrison.schroeder@kellogg.northwestern.edu>
2025-06-04 15:09:59 +0000

    Forces fields to be completed

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,6 @@
 class Entry < ApplicationRecord
   has_one_attached :uploaded_image
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :occurred_on, presence: true
 end
